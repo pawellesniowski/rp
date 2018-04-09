ecdc80ad3a1609b1b2d6205980098c725633f0a7
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-04-09 09:41:49 +0200

    raw components for general Tab

--- src/pages/inside/settingsPage/generalTab/generalTab.jsx
+++ src/pages/inside/settingsPage/generalTab/generalTab.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Input } from '../../../../components/inputs/input';
 
 import styles from './generalTab.scss';
+import {InputDropdown} from "../../../../components/inputs/inputDropdown";
+import {InputBigSwitcher} from "../../../../components/inputs/inputBigSwitcher";
+import {BigButton} from "../../../../components/buttons/bigButton";
+
 
 const options = [
   {
@@ -41,7 +45,27 @@ export class GeneralTab extends Component {
           <Input disabled placeholder="artem" />
 
           <label className={styles.generalTabLabel}>Launch inactivity timeout</label>
-          <Input disabled placeholder="artem" />
+          <InputDropdown className={styles.generalTabDropDown} options={options} />
+          <p>Schedule time for Job to interrupt inactive launches</p>
+
+          <label className={styles.generalTabLabel}>Keep logs</label>
+          <InputDropdown className={styles.generalTabDropDown} options={options} />
+          <p>How long to keep old logs in launches.
+            Related launches structure will be saved, in order to keep statistics
+          </p>
+
+          <label className={styles.generalTabLabel}>Keep attachments</label>
+          <InputDropdown className={styles.generalTabDropDown} options={options} />
+          <p>How long to keep attachments in system</p>
+
+          <label className={styles.generalTabLabel}>Auto analysis</label>
+          <InputBigSwitcher value />
+          <p>If ON - analysis starts as soon as any launch finished. </p>
+          <p>If OFF - not automatic, but can be invoked manually.</p>
+
+          <label className={styles.generalTabLabel}>Base for Auto Analysis</label>
+
+          <BigButton className={styles.bigButton}>Submit</BigButton>
 
         </form>
 
